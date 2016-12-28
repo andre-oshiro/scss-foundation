29118c13af4903cc510e5c3e9549459829199619
TSHiYK <toshiyuki.i1223+dev@gmail.com>
2016-12-29 08:22:06 +0900

    Add a url-encode function

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
 const sassTrue = require('sass-true');
 
-const sassFile = path.join(__dirname, '_replace.scss');
+const sassFile = path.join(__dirname, 'test.scss');
 sassTrue.runSass({ file: sassFile }, describe, it);
